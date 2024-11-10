987955ee872d467d84173ac57af569b4de80219e
Brad Schick <schickb@gmail.com>
2024-11-10 03:15:49 +0000

    increased test timeout for pokey firefox

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -16,7 +16,7 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        DEFAULT_TIMEOUT_INTERVAL: 10000,
+        timeoutInterval: 20000
         // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
