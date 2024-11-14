f0aa87e1449180fba86f2d3482efaa6d3b8c5087
Brad Schick <schickb@gmail.com>
2024-11-14 00:42:08 +0000

    seperated stream logic so simplify cmdline tool, which allowed for other cleanup

---
+++ src/app/services/cipher-streams.ts
@@ -0,0 +1,195 @@
+/* MIT License
+
+Copyright (c) 2024 Brad Schick
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+import * as cc from './cipher.consts';
+import {
+   Ciphers,
+   Encipher,
+   Decipher,
+   EParams,
+   CipherDataInfo,
+   PWDProvider
+} from './ciphers';
+import { browserSupportsBytesStream, streamWriteBYOD } from './utils';
+
+
+export {CipherDataInfo, PWDProvider};
+
+export type EContext = {
+   readonly alg: string;
+   readonly ic: number;
+   readonly trueRand: boolean;
+   readonly fallbackRand: boolean;
+   readonly lpEnd: number;
+};
+
+
+export async function encryptStream(
+   econtext: EContext,
+   pwdProvider: PWDProvider,
+   userCred: Uint8Array,
+   clearStream: ReadableStream<Uint8Array>
+): Promise<ReadableStream<Uint8Array>> {
+   return _encryptStreamImpl(
+      econtext,
+      pwdProvider,
+      userCred,
+      clearStream,
+      1
+   );
+}
+
+async function _encryptStreamImpl(
+   econtext: EContext,
+   pwdProvider: PWDProvider,
+   userCred: Uint8Array,
+   clearStream: ReadableStream<Uint8Array>,
+   lp: number
+): Promise<ReadableStream<Uint8Array>> {
+
+   if (!Ciphers.validateAlg(econtext.alg)) {
+      throw new Error('Invalid alg of: ' + econtext.alg);
+   }
+   if (econtext.ic < cc.ICOUNT_MIN || econtext.ic > cc.ICOUNT_MAX) {
+      throw new Error('Invalid ic of: ' + econtext.ic);
+   }
+   if (econtext.lpEnd > cc.LP_MAX) {
+      throw new Error('Invalid loop end of: ' + econtext.lpEnd);
+   }
+   if (lp < 1 || lp > econtext.lpEnd) {
+      throw new Error('Invalid loop of: ' + lp);
+   }
+   if (!econtext.trueRand && !econtext.fallbackRand) {
+      throw new Error('Either trueRand or fallbackRand must be true');
+   }
+   if (userCred.byteLength != cc.USERCRED_BYTES) {
+      throw new Error('Invalid userCred length of: ' + userCred.byteLength);
+   }
+
+   const encipher = Encipher.latest(userCred, clearStream);
+
+   const eparams: EParams = {
+      ...econtext,
+      lp
+   };
+
+   let cipherStream = new ReadableStream({
+      type: (browserSupportsBytesStream() ? 'bytes' : undefined),
+
+      async pull(controller) {
+
+         try {
+            const cipherData = await encipher.encryptBlock(eparams, pwdProvider);
+
+            if (cipherData.parts.length) {
+               for (let data of cipherData.parts) {
+                  streamWriteBYOD(controller, data);
+               }
+            }
+
+            if (cipherData.done) {
+               controller.close();
+               // See: https://stackoverflow.com/questions/78804588/why-does-read-not-return-in-byob-mode-when-stream-is-closed/
+               //@ts-ignore
+               controller.byobRequest?.respond(0);
+            }
+         } catch (err) {
+            controller.error(err);
+         }
+      }
+   });
+
+   if (lp < econtext.lpEnd) {
+      cipherStream = await _encryptStreamImpl(
+         econtext,
+         pwdProvider,
+         userCred,
+         cipherStream,
+         lp + 1
+      );
+   }
+
+   return cipherStream;
+}
+
+
+export async function getCipherStreamInfo(
+   userCred: Uint8Array,
+   cipherStream: ReadableStream<Uint8Array>,
+): Promise<CipherDataInfo> {
+   if (userCred.byteLength != cc.USERCRED_BYTES) {
+      throw new Error('Invalid userCred length of: ' + userCred.byteLength);
+   }
+
+   const decipher = await Decipher.fromStream(userCred, cipherStream);
+   return decipher.getCipherDataInfo();
+}
+
+
+export async function decryptStream(
+   pwdProvider: PWDProvider,
+   userCred: Uint8Array,
+   cipherStream: ReadableStream<Uint8Array>
+): Promise<ReadableStream<Uint8Array>> {
+
+   if (userCred.byteLength != cc.USERCRED_BYTES) {
+      throw new Error('Invalid userCred length of: ' + userCred.byteLength);
+   }
+
+   const decipher = await Decipher.fromStream(userCred, cipherStream);
+   const cdInfo = await decipher.getCipherDataInfo();
+
+   let readableStream = new ReadableStream({
+      type: (browserSupportsBytesStream() ? 'bytes' : undefined),
+
+      async pull(controller) {
+
+         try {
+            const decrypted = await decipher.decryptBlock(pwdProvider);
+
+            if (decrypted.byteLength) {
+               streamWriteBYOD(controller, decrypted);
+            } else {
+               // Reached the end of the stream peacefully...
+               controller.close();
+               // See: https://stackoverflow.com/questions/78804588/why-does-read-not-return-in-byob-mode-when-stream-is-closed/
+               //@ts-ignore
+               controller.byobRequest?.respond(0);
+            }
+
+         } catch (err) {
+            controller.error(err);
+         }
+      }
+
+   });
+
+   if (cdInfo.lp > 1) {
+      readableStream = await decryptStream(
+         pwdProvider,
+         userCred,
+         readableStream
+      );
+   }
+
+   return readableStream
+}
