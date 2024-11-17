5a2f4137aa6aeb70684efa4fa80c35b2a9307403
Brad Schick <schickb@gmail.com>
2024-11-17 09:43:14 +0000

    various doc changes

--- src/app/core/core.dialogs.ts
+++ src/app/core/core.dialogs.ts
@@ -41,7 +41,6 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Router } from '@angular/router';
