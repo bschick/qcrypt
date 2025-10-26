d9c70f50b6b8064a0c033bb9f75fbafa5987f944
Brad Schick <schickb@gmail.com>
2025-10-26 22:37:37 +0000

    move stuff around to simplify github actions

--- localssl/localssl.sh
+++ localssl/localssl.sh
@@ -3,6 +3,29 @@
 #https://betterprogramming.pub/how-to-create-trusted-ssl-certificates-for-your-local-development-13fd5aad29c6
 
 HOSTNAME="t1.quickcrypt.org"
+HOSTS_FILE="/etc/hosts"
+
+if grep "^127\.0\.0\.1" "$HOSTS_FILE" | grep -qw "$HOSTNAME"; then
+    echo "-> '$HOSTNAME' already exists on 127.0.0.1 line. Skipping."
+else
+    # If it doesn't, use 'sed' to append it to the end of that specific line
+    echo "-> Appending '$HOSTNAME' to 127.0.0.1 line..."
+    sudo sed -i -E "/^127\.0\.0\.1/ s/$/ $HOSTNAME/" "$HOSTS_FILE"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to update 127.0.0.1 line. Do you have sudo permissions?"
+    fi
+fi
+
+if grep "^::1" "$HOSTS_FILE" | grep -qw "$HOSTNAME"; then
+    echo "-> '$HOSTNAME' already exists on ::1 line. Skipping."
+else
+    # If it doesn't, use 'sed' to append it to the end of that specific line
+    echo "-> Appending '$HOSTNAME' to ::1 line..."
+    sudo sed -i -E "/^::1/ s/$/ $HOSTNAME/" "$HOSTS_FILE"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to update ::1 line. (This is non-fatal if you don't use IPv6)."
+    fi
+fi
 
 openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 \
  -x509 -nodes -days 365 -out qcrypt.pem -keyout qcrypt.key \
@@ -40,29 +63,10 @@ else
     exit 1
 fi
 
+sudo cp qcrypt.crt /usr/local/share/ca-certificates/.
+sudo update-ca-certificates
 
-HOSTS_FILE="/etc/hosts"
-
-if grep "^127\.0\.0\.1" "$HOSTS_FILE" | grep -qw "$HOSTNAME"; then
-    echo "-> '$HOSTNAME' already exists on 127.0.0.1 line. Skipping."
-else
-    # If it doesn't, use 'sed' to append it to the end of that specific line
-    echo "-> Appending '$HOSTNAME' to 127.0.0.1 line..."
-    sudo sed -i -E "/^127\.0\.0\.1/ s/$/ $HOSTNAME/" "$HOSTS_FILE"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to update 127.0.0.1 line. Do you have sudo permissions?"
-    fi
-fi
-
-if grep "^::1" "$HOSTS_FILE" | grep -qw "$HOSTNAME"; then
-    echo "-> '$HOSTNAME' already exists on ::1 line. Skipping."
-else
-    # If it doesn't, use 'sed' to append it to the end of that specific line
-    echo "-> Appending '$HOSTNAME' to ::1 line..."
-    sudo sed -i -E "/^::1/ s/$/ $HOSTNAME/" "$HOSTS_FILE"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to update ::1 line. (This is non-fatal if you don't use IPv6)."
-    fi
-fi
+mkdir -p "$HOME/.pki/nssdb"
+certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "My Custom CA" -i qcrypt.crt
 
 echo "Done."
