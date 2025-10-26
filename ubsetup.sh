5bdff1437a367618b236b87ba175625c5530565e
Brad Schick <schickb@gmail.com>
2025-10-26 09:30:12 +0000

    don't need sudo when running as default lxc user

--- ubsetup.sh
+++ ubsetup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-sudo curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-sudo apt-get install -y nodejs=22.20.0-1nodesource1
-sudo apt-mark hold nodejs
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+apt-get install -y nodejs=22.20.0-1nodesource1
+apt-mark hold nodejs
 
-sudo npm install -g npm@latest
+npm install -g npm@latest
 
 npm install
 
 cd localssl
 ./localssl.sh
 
-sudo cp qcrypt.crt /usr/local/share/ca-certificates/.
-sudo update-ca-certificates
+cp qcrypt.crt /usr/local/share/ca-certificates/.
+update-ca-certificates
 
 mkdir -p "$HOME/.pki/nssdb"
 certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "My Custom CA" -i qcrypt.crt
 
 cd ..
 
-sudo npx playwright install-deps
-sudo npx playwright install
+npx playwright install-deps
+npx playwright install
 
 echo "Done."
