9cc1aa0bb2ad8e0da91347d9da28564946afcb32
Brad Schick <schickb@gmail.com>
2025-10-26 17:07:59 +0000

    put back sudo for no lxc instals

--- ubsetup.sh
+++ ubsetup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+curl apt install -y curl tmux libnss3-tools
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-apt-get install -y nodejs=22.20.0-1nodesource1
-apt-mark hold nodejs
+sudo apt install -y nodejs=22.20.0-1nodesource1
+sudo apt-mark hold nodejs
 
-npm install -g npm@latest
+sudo npm install -g npm@latest
 
 npm install
 
@@ -12,14 +13,14 @@ cd localssl
 ./localssl.sh
 
 cp qcrypt.crt /usr/local/share/ca-certificates/.
-update-ca-certificates
+sudo update-ca-certificates
 
 mkdir -p "$HOME/.pki/nssdb"
 certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "My Custom CA" -i qcrypt.crt
 
 cd ..
 
-npx playwright install-deps
-npx playwright install
+sudo npx playwright install-deps
+sudo npx playwright install
 
 echo "Done."
