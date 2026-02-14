60d2e7c584e50bad2a36093cdafa73be8b4aae0a
Brad Schick <schickb@gmail.com>
2026-02-14 06:22:32 +0000

    exclude spec files

--- apps/web/src/app/services/authenticator.service.ts
+++ apps/web/src/app/services/authenticator.service.ts
@@ -429,6 +429,7 @@ export class AuthenticatorService {
          authenticators: serverUser.authenticators!
       };
 
+      this.refreshSenderLinks();
       this.userInfo.set(userInfo);
       this.activity();
       return userInfo;
