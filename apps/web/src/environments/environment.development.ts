f5afae315c1b6afaa72d01fd1dca634e8025f2d6
Brad Schick <schickb@gmail.com>
2026-03-11 20:02:40 -0700

    don't include package.json in bundle

--- apps/web/src/environments/environment.development.ts
+++ apps/web/src/environments/environment.development.ts
@@ -1,11 +1,9 @@
-import pkg from '../../../../package.json';
+import { shared } from './shared';
 
 export const environment = {
     production: false,
     host: 'https://t1.quickcrypt.org:4200',
     apiHost: 'https://test.quickcrypt.org',
-    apiVersion: 'v1',
     appPublicKey: 'public-key2',
-    clientVersion: pkg.version,
-    copyright: pkg.copyright
+    ...shared
 };
---
+++ apps/web/src/environments/shared.ts
@@ -0,0 +1,5 @@
+export const shared = {
+    apiVersion: 'v1',
+    clientVersion: '7.0.0',
+    copyright: '© 2024–2026 Quick Crypt Contributors'
+};
